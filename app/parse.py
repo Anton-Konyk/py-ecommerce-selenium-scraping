fff1b3a51cafe9a486f79420061d438b0215c5e6
Anton Konyk <antonkonyk.com>
2025-01-16 19:52:59 +0100

    create parse_single_product

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 from urllib.parse import urljoin
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 
 BASE_URL = "https://webscraper.io/"
@@ -20,6 +23,21 @@ class Product:
     rating: int
     num_of_reviews: int
 
+
+def parse_single_product(driver: WebDriver) -> Product:
+
+    return Product(
+        title=driver.find_element(By.CLASS_NAME, "title").
+        get_attribute("title"),
+        description=driver.find_element(By.CLASS_NAME, "description").text,
+        price=float(driver.find_element(By.CLASS_NAME, "price").
+                    text.replace("$", "")),
+        rating=len((driver.find_element(By.CLASS_NAME, "ratings")).
+                   find_elements(By.TAG_NAME, "span")),
+        num_of_reviews=int(
+            driver.find_element(By.CLASS_NAME, "review-count").
+            text.split()[0])
+    )
 
 def get_all_products() -> None:
     pass
