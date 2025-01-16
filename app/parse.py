9ca5a383d4810f4adff0a063cbf1b0a4d0aa8310
Anton Konyk <antonkonyk.com>
2025-01-16 19:50:07 +0100

    create all needed paths

--- app/parse.py
+++ app/parse.py
@@ -3,7 +3,13 @@ from urllib.parse import urljoin
 
 
 BASE_URL = "https://webscraper.io/"
-HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
+HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more")
+COMPUTERS = urljoin(BASE_URL, "test-sites/e-commerce/more/computers")
+LAPTOPS = urljoin(BASE_URL, "test-sites/e-commerce/more/computers/laptops")
+TABLETS = urljoin(BASE_URL, "test-sites/e-commerce/more/computers/tablets")
+PHONES = urljoin(BASE_URL, "test-sites/e-commerce/more/phones")
+TOUCH = urljoin(BASE_URL, "test-sites/e-commerce/more/phones/touch")
+ALL_PAGES = [HOME_URL, COMPUTERS, LAPTOPS, TABLETS, PHONES, TOUCH]
 
 
 @dataclass
